0b5c1312c7da462fcd78db2ed24cef04c93c32a9
Mihir Parikh <mihir787@gmail.com>
2015-03-12 18:17:41 -0600

    transaction_repository_test updated

--- sales_engine/test/transaction_repository_test.rb
+++ sales_engine/test/transaction_repository_test.rb
@@ -1,149 +1,93 @@
 require_relative 'test_helper'
 require_relative '../lib/transaction_repository'
+require_relative '../lib/sales_engine'
 
 class TransactionRepositoryTest < Minitest::Test
-  # def test_returns_all_item_transactions
-  #   transaction1 = Transaction.new(:id => "999")
-  #   transaction2 = Transaction.new(:id => "888")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1, transaction2 ], transaction_repo.all
-  # end
-  #
-  # def test_returns_random_transaction
-  #   transaction1 = Transaction.new(:id => "123")
-  #   transaction2 = Transaction.new(:id => "321")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   sample = []
-  #   sample << transaction_repo.random
-  #   assert_equal 1, sample.length
-  # end
-  #
-  # def test_returns_transaction_item_with_matching_id
-  #   transaction1 = Transaction.new(:id => "5321")
-  #   transaction2 = Transaction.new(:id => "1234")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal transaction1, transaction_repo.find_by_id("5321")
-  # end
-  #
-  # def test_returns_transaction_item_with_matching_invoice_id
-  #   transaction1 = Transaction.new(:invoice_id => "5321")
-  #   transaction2 = Transaction.new(:invoice_id => "1234")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  transaction2 , transaction_repo.find_by_invoice_id("1234")
-  # end
-  #
-  # def test_returns_transaction_item_with_matching_credit_card_number
-  #   transaction1 = Transaction.new(:credit_card_number => "12312412")
-  #   transaction2 = Transaction.new(:credit_card_number => "643523413")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  transaction2 , transaction_repo.find_by_credit_card_number("643523413")
-  # end
-  #
-  # def test_returns_transaction_item_with_matching_credit_card_expiration_date
-  #   transaction1 = Transaction.new(:credit_card_expiration_date => "01/10")
-  #   transaction2 = Transaction.new(:credit_card_expiration_date => "02/12")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal transaction1, transaction_repo.find_by_credit_card_expiration_date("01/10")
-  # end
-  #
-  # def test_returns_transaction_item_with_matching_result
-  #   transaction1 = Transaction.new(:result => "25")
-  #   transaction2 = Transaction.new(:result => "30")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  transaction2 , transaction_repo.find_by_result("30")
-  # end
-  #
-  # def test_returns_transaction_with_matching_created_at_date
-  #   transaction1 = Transaction.new(:created_at => "12/12/12")
-  #   transaction2 = Transaction.new(:created_at => "02/02/02")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  transaction2 , transaction_repo.find_by_created_at("02/02/02")
-  # end
-  #
-  # def test_returns_transaction_with_matching_updated_at_date
-  #   transaction1 = Transaction.new(:updated_at => "04/02/12")
-  #   transaction2 = Transaction.new(:updated_at => "10/05/15")
-  #   transactions = [transaction1,  transaction2 ]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  transaction2 , transaction_repo.find_by_updated_at("10/05/15")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_id
-  #   transaction1 = Transaction.new(:id => "5321")
-  #   transaction2 = Transaction.new(:id => "1234")
-  #   transaction3 = Transaction.new(:id => "9999")
-  #   transaction4 = Transaction.new(:id => "1234")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  [transaction2 ,transaction4], transaction_repo.find_all_by_id("1234")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_invoice_id
-  #   transaction1 = Transaction.new(:invoice_id => "1234")
-  #   transaction2 = Transaction.new(:invoice_id => "5321")
-  #   transaction3 = Transaction.new(:invoice_id => "1234")
-  #   transaction4 = Transaction.new(:invoice_id => "9999")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1,transaction3], transaction_repo.find_all_by_invoice_id("1234")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_result
-  #   transaction1 = Transaction.new(:result => "sent")
-  #   transaction2 = Transaction.new(:result => "sent")
-  #   transaction3 = Transaction.new(:result => "fail")
-  #   transaction4 = Transaction.new(:result => "fail")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal  [transaction1 ,transaction2], transaction_repo.find_all_by_result("sent")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_credit_card_number
-  #   transaction1 = Transaction.new(:credit_card_number => "10012002")
-  #   transaction2 = Transaction.new(:credit_card_number => "20021001")
-  #   transaction3 = Transaction.new(:credit_card_number => "30034004")
-  #   transaction4 = Transaction.new(:credit_card_number => "10012002")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1,transaction4], transaction_repo.find_all_by_credit_card_number("10012002")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_credit_card_expiration_date
-  #   transaction1 = Transaction.new(:credit_card_expiration_date => "05/14")
-  #   transaction2 = Transaction.new(:credit_card_expiration_date => "05/14")
-  #   transaction3 = Transaction.new(:credit_card_expiration_date => "12/12")
-  #   transaction4 = Transaction.new(:credit_card_expiration_date => "11/12")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1,transaction2], transaction_repo.find_all_by_credit_card_expiration_date("05/14")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_created_at
-  #   transaction1 = Transaction.new(:created_at => "05/05/95")
-  #   transaction2 = Transaction.new(:created_at => "05/05/95")
-  #   transaction3 = Transaction.new(:created_at => "05/10/95")
-  #   transaction4 = Transaction.new(:created_at => "05/05/95")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1, transaction2 ,transaction4], transaction_repo.find_all_by_created_at("05/05/95")
-  # end
-  #
-  # def test_returns_all_invoice_item_with_matching_updated_at
-  #   transaction1 = Transaction.new(:updated_at => "05/10/95")
-  #   transaction2 = Transaction.new(:updated_at => "05/05/95")
-  #   transaction3 = Transaction.new(:updated_at => "05/10/95")
-  #   transaction4 = Transaction.new(:updated_at => "05/05/95")
-  #   transactions = [transaction1,  transaction2 , transaction3, transaction4]
-  #   transaction_repo = TransactionRepository.new(transactions)
-  #   assert_equal [transaction1,transaction3], transaction_repo.find_all_by_updated_at("05/10/95")
-  # end
+  attr_reader :sales_engine
+  def setup
+    @sales_engine = SalesEngine.new("./fixtures")
+    @sales_engine.startup
+  end
+
+  def test_returns_all_item_transactions
+    assert_equal 6, sales_engine.transaction_repository.all.count
+  end
+
+  def test_returns_random_transaction
+    sample = []
+    sample << sales_engine.transaction_repository.random
+    assert_equal 1, sample.length
+  end
+
+  def test_returns_transaction_item_with_matching_id
+    result = sales_engine.transaction_repository.find_by_id("5591")
+    assert_equal "5591", result.id
+  end
+
+  def test_returns_transaction_item_with_matching_invoice_id
+    result = sales_engine.transaction_repository.find_by_invoice_id("4842")
+    assert_equal  "4842" , result.invoice_id
+  end
+
+  def test_returns_transaction_item_with_matching_credit_card_number
+    result = sales_engine.transaction_repository.find_by_credit_card_number("4397199533314394")
+    assert_equal  "4397199533314394", result.credit_card_number
+  end
+
+  def test_returns_transaction_item_with_matching_credit_card_expiration_date
+    result = sales_engine.transaction_repository.find_by_credit_card_expiration_date(nil)
+    assert_equal nil, result.credit_card_expiration_date
+  end
+
+  def test_returns_transaction_item_with_matching_result
+    result = sales_engine.transaction_repository.find_by_result("failed")
+    assert_equal  "5591" , result.id
+  end
+
+  def test_returns_transaction_with_matching_created_at_date
+    result = sales_engine.transaction_repository.find_by_created_at("2012-03-27 14:58:15 UTC")
+    assert_equal "5591", result.id
+  end
+
+  def test_returns_transaction_with_matching_updated_at_date
+    result = sales_engine.transaction_repository.find_by_updated_at("2012-03-27 14:58:15 UTC")
+    assert_equal "5591", result.id
+  end
+
+  def test_returns_all_invoice_item_with_matching_id
+    result = sales_engine.transaction_repository.find_all_by_id("5593")
+    assert_equal 1, result.count
+  end
+
+  def test_returns_all_invoice_item_with_matching_invoice_id
+    result = sales_engine.transaction_repository.find_all_by_invoice_id("4842")
+    assert_equal 2, result.count
+  end
+
+  def test_returns_all_invoice_item_with_matching_result
+    result = sales_engine.transaction_repository.find_all_by_result("success")
+    assert_equal 4, result.count
+  end
+
+  def test_returns_all_invoice_item_with_matching_credit_card_number
+    result = sales_engine.transaction_repository.find_all_by_credit_card_number("4680790942233742")
+    assert_equal 1, result.count
+    assert_equal "4680790942233742", result[0].credit_card_number
+  end
+
+  def test_returns_all_invoice_item_with_matching_credit_card_expiration_date
+    result = sales_engine.transaction_repository.find_all_by_credit_card_expiration_date(nil)
+    assert_equal 6, result.count
+  end
+
+  def test_returns_all_invoice_item_with_matching_created_at
+    result = sales_engine.transaction_repository.find_all_by_created_at("2012-03-27 14:58:15 UTC")
+    assert_equal 5, result.count
+  end
+
+  def test_returns_all_invoice_item_with_matching_updated_at
+    result = sales_engine.transaction_repository.find_all_by_updated_at("2012-03-27 14:58:15 UTC")
+    assert_equal 5, result.count
+  end
 
 end
--- sales_engine/test/transaction_test.rb
+++ sales_engine/test/transaction_test.rb
@@ -1,41 +1,40 @@
 require_relative 'test_helper'
 require_relative '../lib/transaction'
+require_relative '../lib/sales_engine'
 
 class TransactionTest < Minitest::Test
 
   def setup
-    data =  {id: "7", invoice_id: "8", credit_card_number: "4801647818676136",
-      credit_card_expiration: "", result: "success", created_at: "2012-03-27 14:54:10 UTC",
-      updated_at: "2012-03-27 14:54:10 UTC" }
-
-    @transaction = Transaction.new(data)
+    @sales_engine = SalesEngine.new("./fixtures")
+    @sales_engine.startup
   end
 
-  def test_id
-    assert_equal "7", @transaction.id
+  def test_returns_cusomter_by
+    result = @sales_engine.transaction_repository.find_by_id("5591")
+    assert_equal "7", result
   end
 
-  def test_invoice_id
-    assert_equal "8", @transaction.invoice_id
-  end
-
-  def test_credit_card_number
-    assert_equal "4801647818676136", @transaction.credit_card_number
-  end
-
-  def test_credit_card_expiration
-    assert_equal nil, @transaction.credit_card_expiration_date
-  end
-
-  def test_result
-    assert_equal "success", @transaction.result
-  end
-
-  def test_created_at
-    assert_equal "2012-03-27 14:54:10 UTC", @transaction.created_at
-  end
-
-  def test_updated_at
-    assert_equal "2012-03-27 14:54:10 UTC", @transaction.updated_at
-  end
+  # def test_invoice_id
+  #   assert_equal "8", @transaction.invoice_id
+  # end
+  #
+  # def test_credit_card_number
+  #   assert_equal "4801647818676136", @transaction.credit_card_number
+  # end
+  #
+  # def test_credit_card_expiration
+  #   assert_equal nil, @transaction.credit_card_expiration_date
+  # end
+  #
+  # def test_result
+  #   assert_equal "success", @transaction.result
+  # end
+  #
+  # def test_created_at
+  #   assert_equal "2012-03-27 14:54:10 UTC", @transaction.created_at
+  # end
+  #
+  # def test_updated_at
+  #   assert_equal "2012-03-27 14:54:10 UTC", @transaction.updated_at
+  # end
 end
