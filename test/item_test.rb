d2ca5a83f86044b7d3188b97253293c263acbc41
Mihir Parikh <mihir787@gmail.com>
2015-03-16 17:50:50 -0600

    item tests passing

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,6 +1,8 @@
 require_relative 'test_helper'
 require_relative "../lib/item"
 require_relative "../lib/sales_engine"
+require 'bigdecimal'
+require 'bigdecimal/util'
 
 class ItemTest < MiniTest::Test
   attr_reader :sales_engine
@@ -15,44 +17,44 @@ class ItemTest < MiniTest::Test
 
 
   def test_id
-    result = sales_engine.item_repository.find_by_id("2481")
-    assert_equal "2481", result.id
+    result = sales_engine.item_repository.find_by_id(2481)
+    assert_equal 2481, result.id
   end
 
   def test_name
-    result = sales_engine.item_repository.find_by_id("2481")
+    result = sales_engine.item_repository.find_by_id(2481)
     assert_equal "Item Sit Esse", result.name
   end
 
   def test_description
-    result = sales_engine.item_repository.find_by_id("2481")
+    result = sales_engine.item_repository.find_by_id(2481)
     assert_equal "Natus soluta qui consequatur repellat beatae aspernatur. Qui fuga sed velit. Vitae rerum suscipit quidem sed unde.", result.description
   end
 
   def test_unit_price
-    result = sales_engine.item_repository.find_by_id("2481")
-    assert_equal "92637", result.unit_price
+    result = sales_engine.item_repository.find_by_id(2481)
+    assert_equal (92637.to_d)/100, result.unit_price
   end
 
   def test_merchant_id
-    result = sales_engine.item_repository.find_by_id("2481")
-    assert_equal "100", result.merchant_id
+    result = sales_engine.item_repository.find_by_id(2481)
+    assert_equal 100, result.merchant_id
   end
 
   def test_created_at
-    result = sales_engine.item_repository.find_by_id("2481")
-    assert_equal "2012-03-27 14:54:09 UTC", result.created_at
+    result = sales_engine.item_repository.find_by_id(2481)
+    assert_equal 27, result.created_at.mday
   end
 
   def test_updated_at
-    result = sales_engine.item_repository.find_by_id("2481")
-    assert_equal "2012-03-27 14:54:09 UTC", result.updated_at
+    result = sales_engine.item_repository.find_by_id(2481)
+    assert_equal 27, result.updated_at.mday
   end
 
   def test_it_can_call_up_to_repository_with_item_id
     parent = Minitest::Mock.new
     item = Item.new(@data, parent)
-    parent.expect(:find_invoice_items, ["pizza", "burgers"], ["1"])
+    parent.expect(:find_invoice_items, ["pizza", "burgers"], [1])
     assert_equal ["pizza", "burgers"], item.invoice_items
     parent.verify
   end
@@ -60,7 +62,7 @@ class ItemTest < MiniTest::Test
   def test_it_can_call_up_to_repository_with_merchant_id
     parent = Minitest::Mock.new
     item = Item.new(@data, parent)
-    parent.expect(:find_merchant, ["michael"], ["456"])
+    parent.expect(:find_merchant, ["michael"], [456])
     assert_equal ["michael"], item.merchant
     parent.verify
   end
